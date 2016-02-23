92fe33ae38570b0c4775817c4287fad4dda1fdec
Paul Anderson <hpcprofessional@gmail.com>
2016-02-23 16:48:10 -0700

    Homework 11.2 - explicit ordering

--- site/nginx/manifests/init.pp
+++ site/nginx/manifests/init.pp
@@ -2,6 +2,7 @@ class nginx {
 
   package { 'nginx':
     ensure => present,
+    before => [File['config'],File['block']],
   }
   
   file { 'docroot':
@@ -13,21 +14,28 @@ class nginx {
     ensure => file,
     path => '/var/www/index.html',
     source => 'puppet:///modules/nginx/index.html',
+  }
     
   file { 'config':
     ensure => file,
     path => '/etc/nginx/nginx.conf',
     source => 'puppet:///modules/nginx/nginx.conf',
+    require => Package['nginx'],
+    notify => Service['nginx'],
   }
   
   file { 'block':
     ensure => file,
     path => '/etc/nginx/conf.d/default.conf',
     source => 'puppet:///modules/nginx/default.conf',
+    require => Package['nginx'],
+    notify => Service['nginx'],
   }
 
   service { 'nginx':
     ensure => running,
+    require => [File['docroot'],File['index']],
+    subscribe => [File['config'],File['block']],
   }
 
 }
