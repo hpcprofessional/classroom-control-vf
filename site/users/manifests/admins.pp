5f42cb83b93446ea0bd363837dff1bd55ef51359
Paul Anderson <hpcprofessional@gmail.com>
2016-02-24 15:40:16 -0700

    15.1 Using correct defined resource type name

--- site/users/manifests/admins.pp
+++ site/users/manifests/admins.pp
@@ -1,13 +1,13 @@
 class users::admins {
 
-  users::managed_users { 'paul': 
+  users::managed_user { 'paul': 
     home_base => '/tmp',
   }
-  users::managed_users { 'jose': }
-  users::managed_users { 'alice': 
+  users::managed_user { 'jose': }
+  users::managed_user { 'alice': 
     group => 'devops',
   }
-  users::managed_users { 'chen': 
+  users::managed_user { 'chen': 
     group => 'appdev',
     home_base => '/appdev',
   }
