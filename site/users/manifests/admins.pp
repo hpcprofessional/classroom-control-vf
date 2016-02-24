53355f62b5bd00c9750da325054c6c2d2f412526
Paul Anderson <hpcprofessional@gmail.com>
2016-02-24 15:37:02 -0700

    Lab 15.1 Better syntax

--- site/users/manifests/admins.pp
+++ site/users/manifests/admins.pp
@@ -1,15 +1,15 @@
 class users::admins {
 
   users::managed_users { 'paul': 
-    $home_base = '/tmp',
+    home_base => '/tmp',
   }
   users::managed_users { 'jose': }
   users::managed_users { 'alice': 
-    $group = 'devops',
+    group => 'devops',
   }
   users::managed_users { 'chen': 
-    $group = 'appdev',
-    $home_base = '/appdev'
+    group => 'appdev',
+    home_base => '/appdev',
   }
 
   file { "/appdev":
