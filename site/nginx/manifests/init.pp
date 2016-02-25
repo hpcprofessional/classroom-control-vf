c39a36e1c373f53c23015d50d3c590e287c6b7a4
Paul Anderson <hpcprofessional@gmail.com>
2016-02-25 12:08:06 -0700

    Lab 18.1 - parameterize root/docroot

--- site/nginx/manifests/init.pp
+++ site/nginx/manifests/init.pp
@@ -1,11 +1,13 @@
-class nginx{
+class nginx (
+  $root = undef,
+) {
 
 case $::osfamily {
   'RedHat' : {
     $package  = 'nginx'
     $owner    = 'root'
     $group    = 'root'
-    $docroot  = '/var/www'
+    $default_docroot  = '/var/www'
     $confdir  = '/etc/nginx'
     $blockdir = '/etc/nginx/conf.d'
     $logdir   = '/var/log/nginx'
@@ -16,7 +18,7 @@ case $::osfamily {
     $package  = 'nginx'
     $owner    = 'root'
     $group    = 'root'
-    $docroot  = '/var/www'
+    $default_docroot  = '/var/www'
     $confdir  = '/etc/nginx'
     $blockdir = '/etc/nginx/conf.d'
     $logdir   = '/var/log/nginx'
@@ -27,7 +29,7 @@ case $::osfamily {
     $package  = 'nginx-service'
     $owner    = 'Administrator'
     $group    = 'Administrators'
-    $docroot  = 'C:/ProgramData/nginx/html'
+    $default_docroot  = 'C:/ProgramData/nginx/html'
     $confdir  = 'C:/ProgramData/nginx'
     $blockdir = 'C:/ProgramData/nginx/conf.d'
     $logdir   = 'C:/ProgramData/nginx/logs'
@@ -37,6 +39,11 @@ case $::osfamily {
   default : {
     fail { "Get a suppported Operating System, friend.": }
   }
+}
+
+$docroot = $root ? {
+  undef => $default_docroot,
+  default => $root,
 }
 
   File {
