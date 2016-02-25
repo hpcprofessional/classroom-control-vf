e98dfe5a70613bfedb85b4cc38da83ae1bc1f649
Paul Anderson <hpcprofessional@gmail.com>
2016-02-24 17:02:05 -0700

    Lab 15.2 Better case statement.

--- site/nginx/manifests/init.pp
+++ site/nginx/manifests/init.pp
@@ -2,37 +2,37 @@ class nginx{
 
 case $::osfamily {
   'RedHat' : {
-    $package  = 'nginx',
-    $owner    = 'root',
-    $group    = 'root',
-    $docroot  = '/var/www',
-    $confdir  = '/etc/nginx',
-    $blockdir = '/etc/nginx/conf.d',
-    $logdir   = '/var/log/nginx',
-    $service  = 'nginx',
-    $user     = 'nginx',
+    $package  = 'nginx'
+    $owner    = 'root'
+    $group    = 'root'
+    $docroot  = '/var/www'
+    $confdir  = '/etc/nginx'
+    $blockdir = '/etc/nginx/conf.d'
+    $logdir   = '/var/log/nginx'
+    $service  = 'nginx'
+    $user     = 'nginx'
   }
   'Debian' : {
-    $package  = 'nginx',
-    $owner    = 'root',
-    $group    = 'root',
-    $docroot  = '/var/www',
-    $confdir  = '/etc/nginx',
-    $blockdir = '/etc/nginx/conf.d',
-    $logdir   = '/var/log/nginx',
-    $service  = 'nginx',
-    $user     = 'www-data',
+    $package  = 'nginx'
+    $owner    = 'root'
+    $group    = 'root'
+    $docroot  = '/var/www'
+    $confdir  = '/etc/nginx'
+    $blockdir = '/etc/nginx/conf.d'
+    $logdir   = '/var/log/nginx'
+    $service  = 'nginx'
+    $user     = 'www-data'
   }
   'windows' : {
-    $package  = 'nginx-service',
-    $owner    = 'Administrator',
-    $group    = 'Administrators',
-    $docroot  = 'C:/ProgramData/nginx/html',
-    $confdir  = 'C:/ProgramData/nginx',
-    $blockdir = 'C:/ProgramData/nginx/conf.d',
-    $logdir   = 'C:/ProgramData/nginx/logs',
-    $service  = 'nginx',
-    $user     = 'nobody',
+    $package  = 'nginx-service'
+    $owner    = 'Administrator'
+    $group    = 'Administrators'
+    $docroot  = 'C:/ProgramData/nginx/html'
+    $confdir  = 'C:/ProgramData/nginx'
+    $blockdir = 'C:/ProgramData/nginx/conf.d'
+    $logdir   = 'C:/ProgramData/nginx/logs'
+    $service  = 'nginx'
+    $user     = 'nobody'
   }
   default : {
     fail { "Get a suppported Operating System, friend.": }
