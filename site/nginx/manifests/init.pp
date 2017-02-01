89565d4c6e753332db63dc2e4c45ce1c00f9e29e
Joseph Oaks <josephoaks@users.noreply.github.com>
2017-02-01 13:26:12 -0700

    Create init.pp

---
+++ site/nginx/manifests/init.pp
@@ -0,0 +1,47 @@
+class nginx {
+  package { 'nginx':
+    ensure => present,
+    before => [ File['nginx.conf'], File['default.conf'] ],
+  }
+  
+  file { 'docroot':
+    ensure => directory,
+    owner  => 'root',
+    group  => 'root',
+    path   => '/var/www',
+  }
+  
+  file { 'index.html':
+    ensure => file,
+    owner  => 'root',
+    group  => 'root',
+    mode   => '0664',
+    path   => '/var/www/index.html',
+    source => 'puppet:///modules/nginx/index.html',
+  }
+  
+  file { 'nginx.conf':
+    ensure => file,
+    owner  => 'root',
+    group  => 'root',
+    mode   => '0664',
+    path   => '/etc/nginx/nginx.conf',
+    source => 'puppet:///modules/nginx/nginx.conf',
+  }
+  
+  file { 'default.conf':
+    ensure => file,
+    owner  => 'root',
+    group  => 'root',
+    mode   => '0664',
+    path   => '/etc/nginx/conf.d/default.conf',
+    source => 'puppet:///modules/nginx/default.conf',
+  }
+  
+  service { 'nginx':
+    ensure => running,
+    enable => true,
+    subscribe => [ File['nginx.conf'], File['default.conf'],
+  }
+
+}
