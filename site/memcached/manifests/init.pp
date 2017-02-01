459c0204238a5035f2f40009b5e2f2c67a5f9d38
Joseph Oaks <josephoaks@users.noreply.github.com>
2017-02-01 11:38:40 -0700

    Update init.pp

--- site/memcached/manifests/init.pp
+++ site/memcached/manifests/init.pp
@@ -1,4 +1,21 @@
 class memcached {
-
-
+  package { 'memcached':
+    ensure => present,
+    before => File['memcached'],
+  }
+  
+  file { 'memcached':
+    ensure => file,
+    owner  => 'root',
+    group  => 'root',
+    mode   => '0664',
+    path   => '/etc/sysconfig/memcached',
+    source => 'puppet:///modules/memcached/memcached',
+  }
+  
+  service { 'memcached':
+    ensure => running,
+    enable => true
+    subscribe => File['memcached'],
+  }
 }
